3c9098a64b1fee702be58691219f29c599eca5b7
Krzysztof Baczyński <k.baczynski200@gmail.com>
2024-11-17 19:35:37 +0100

    mailconfig

--- src/main/java/com/capgemini/wsb/fitnesstracker/mail/internal/MailConfig.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/mail/internal/MailConfig.java
@@ -1,10 +1,24 @@
 package com.capgemini.wsb.fitnesstracker.mail.internal;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 @EnableConfigurationProperties(MailProperties.class)
 class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("przykladowy@gmail.com");
+        mailSender.setPassword("haslo");
+
+        return mailSender;
+    }
 
 }
